e6b2c72ffdd7974b77aa44fa136a2ddf2243246f
Hieu Tran <hieu.tran@enouvo.com>
2022-11-17 14:45:02 +0700

    add 02

--- src/labs/02.ts
+++ src/labs/02.ts
@@ -2,3 +2,8 @@
 let a: number;
 a = 1;
 console.log("check a", a);
+
+// array
+let ArrayString: string[] = ["a", "b", "c"];
+ArrayString.push("abc");
+console.log("check array type string", ArrayString);
